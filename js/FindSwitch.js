c12d9ab22be9c0bd0bc094dfd8c76560001ad9ad
PrjFind <147293817+PrjFind@users.noreply.github.com>
2023-10-08 13:18:53 +0800

    FindSwitch

--- js/FindSwitch.js
+++ js/FindSwitch.js
@@ -1,15 +1,15 @@
 var array = [
-    {"id": 1, "imgName": "P6-1"},
-    {"id": 2, "imgName": "P6-2"},
-    {"id": 3, "imgName": "P6-3"},
-    {"id": 4, "imgName": "SPCU"},
-    {"id": 5, "imgName": "P6-11"},
-    {"id": 6, "imgName": "P6-12"},
-    {"id": 7, "imgName": "P18-1"},
-    {"id": 8, "imgName": "P18-2"},
-    {"id": 9, "imgName": "P18-3"}
-
-
+    {"id": "P6-1-A10", "imgName": "P6-1-A10"},
+    {"id": "P6-1-A12", "imgName": "P6-1-A12"},
+    {"id": "P6-1-A13", "imgName": "P6-1-A13"},
+    {"id": "P6-1-A14", "imgName": "P6-1-A14"},
+    {"id": "P6-1-A15", "imgName": "P6-1-A15"},
+    {"id": "P6-1-A16", "imgName": "P6-1-A16"},
+    {"id": "P6-1-A17", "imgName": "P6-1-A17"},
+    {"id": "P6-1-B4", "imgName": "P6-1-B4"},
+    {"id": "P6-1-B5", "imgName": "P6-1-B5"},
+    {"id": "P6-1-B6", "imgName": "P6-1-B6"},
+    {"id": "P6-1-B7", "imgName": "P6-1-B7"}
 ];
 var imgId;
 var clock;
